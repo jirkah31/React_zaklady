37df9cbf94e1baf62062d031a004f731099d52eb
Jiří Hruška <jirkah.31@seznam.cz>
2023-01-05 11:13:12 +0100

    "add edit note function"

---
+++ záloha/06-notepad-project/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Note from './components/Note';
+
+
+
+const App = () => {
+		return (
+			<div>
+				<Note />
+			</div>
+		)
+	}
+
+export default App;
